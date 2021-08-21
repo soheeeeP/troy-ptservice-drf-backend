83f6054efdf634cb8ba26f6127bcc5d4027220e3
soheeeeP <tpfj@naver.com>
2021-08-22 02:23:18 +0900

    fix authentication-backend err

--- Troy/backend.py
+++ Troy/backend.py
@@ -10,7 +10,7 @@ class PasswordlessBackend(ModelBackend):
         try:
             user = get_user_model().objects.get(email=email)
             return user
-        except get_user_model().user.DoesNotExist:
+        except get_user_model().DoesNotExist:
             return None
 
     def get_user(self, user_pk):
