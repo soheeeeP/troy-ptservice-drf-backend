7dd5a4189d8f0546193260c76883ba2e767e460a
soheeeeP <tpfj@naver.com>
2021-08-22 01:24:22 +0900

    add serializers

--- users/serializer.py
+++ users/serializer.py
@@ -1,12 +1,15 @@
+import os, requests
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import update_last_login
+from django.db import transaction
 
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import UserProfile, TraineeProfile, TrainerProfile
+from .models import UserProfile, TraineeProfile, TrainerProfile, BodyInfo
+from tags.serializer import HashTagSerializer
+from centers.serializer import CenterSerializer
 from Troy.settings import base
-from Troy.backend import PasswordlessBackend
 
 
 class AuthSerializer(serializers.Serializer):
@@ -45,6 +48,39 @@ class LoginSerializer(serializers.Serializer):
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    # profile_img = serializers.FileField(max_length=None)
+
     class Meta:
         model = UserProfile
+        fields = ['email', 'oauth_type', 'oauth_token', 'username', 'nickname', 'gender', 'birth_year']
+
+    def create(self, validated_data):
+        user, created = UserProfile.objects.update_or_create(
+            email=validated_data.get('email', None),
+            defaults={
+                'oauth_type': validated_data.get('oauth_type', None),
+                'oauth_token': validated_data.get('oauth_token', None),
+                'username': validated_data.get('username', None),
+                'nickname': validated_data.get('nickname', None),
+                'gender': validated_data.get('gender', None),
+                'birth_year': validated_data.get('birth_year', None)
+            }
+        )
+        return user
+
+
+class BodyInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BodyInfo
         fields = '__all__'
+
+    def create(self, validated_data):
+
+        return super(BodyInfoSerializer, self).create(validated_data)
+
+class TrainerProfileSerializer(serializers.ModelSerializer):
+    center = CenterSerializer()
+
+    class Meta:
+        model = TrainerProfile
+        fields = ['center', 'years_career', 'license', 'education']
