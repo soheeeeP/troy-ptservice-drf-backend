63f456ef3bb23d485d928a1f78c98dec0c418041
soheeeeP <tpfj@naver.com>
2021-08-30 17:25:25 +0900

    pass token related urls from users app to oauth app

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-
 from .views import LoginView, SignUpView
 
 app_name = 'users'
 urlpatterns = [
     path('signup', SignUpView.as_view(), name='signup'),
     path('login', LoginView.as_view(), name='login'),
-
-    path('token/obtain',TokenObtainPairView.as_view(), name='token_obtain'),
-    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
-    path('token/refresh', TokenRefreshView.as_view(), name = 'token_refresh')
-
 ]
