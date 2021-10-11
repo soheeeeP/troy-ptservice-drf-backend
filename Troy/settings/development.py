1fc29ce762cbf918fe1547a82b46815842486f2a
soheeeeP <tpfj@naver.com>
2021-10-11 23:45:38 +0900

    SET: celery

--- Troy/settings/development.py
+++ Troy/settings/development.py
@@ -1,4 +1,5 @@
 from .base import *
+from celery.schedules import crontab
 
 DEBUG = True
 # print(getattr(mod, 'django'))
@@ -6,6 +7,8 @@ ALLOWED_HOSTS = getattr(mod, 'django')['hosts']['dev']
 
 INSTALLED_APPS +=[
     'debug_toolbar',
+    'django_celery_results',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE += [
@@ -22,3 +25,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_BEAT_SCHEDULE = {}
+
+DJANGO_CELERY_RESULTS_TASK_ID_MAX_LENGTH = 191
